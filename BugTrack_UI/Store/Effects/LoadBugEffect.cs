32ed72f7a25266ea511a25c98f95765486a06f57
joshua <joshu@JOSHUAPC>
2022-12-10 02:00:57 +0000

    loading gif.

--- BugTrack_UI/Store/Effects/LoadBugEffect.cs
+++ BugTrack_UI/Store/Effects/LoadBugEffect.cs
@@ -22,6 +22,7 @@ namespace BugTrack_UI.Store.Effects
             {
                 dispatcher.Dispatch(new LoadBugsAction());
                 _logger.LogInformation("Loading bugs...");
+                Thread.Sleep(2000);
                 var url = _config.GetValue<string>("APIDetails:APIURI");
                 var client = _httpClient.CreateClient();
                 //throw new Exception("bug/!!");
