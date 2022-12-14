677132cfb4411f40399d7eb6cec33019add0894b
joshua <joshu@JOSHUAPC>
2022-12-14 02:50:44 +0000

    phone validation.

--- Identity/Models/ApplicationUser.cs
+++ Identity/Models/ApplicationUser.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public String? FirstName { get; set; }
+        [Required]
         public String? LastName { get; set; }
     }
 }
